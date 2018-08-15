7ec5471f9d4010b7a369983f88581be177706eb7
bulaimei1991 <bulaimei1991@icloud.com>
2018-08-15 23:32:14 +0800

    change

--- Consecutive Numbers Sum.py
+++ Consecutive Numbers Sum.py
@@ -1,4 +1,24 @@
 # -*- coding=utf-8 -*-
+'''
+给定一个正整数 N，试求有多少组连续正整数满足所有数字之和为 N?
+
+示例 1:
+
+输入: 5
+输出: 2
+解释: 5 = 5 = 2 + 3，共有两组连续整数([5],[2,3])求和后为 5。
+示例 2:
+
+输入: 9
+输出: 3
+解释: 9 = 9 = 4 + 5 = 2 + 3 + 4
+示例 3:
+
+输入: 15
+输出: 4
+解释: 15 = 15 = 8 + 7 = 4 + 5 + 6 = 1 + 2 + 3 + 4 + 5
+说明: 1 <= N <= 10 ^ 9
+'''
 class Solution(object):
     def consecutiveNumbersSum(self, N):
         """
@@ -49,5 +69,26 @@ class Solution(object):
             i = i + 1
         return NumberOfInt
 
+    def consecutiveNumbersSum2(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        import math
+
+        number=0
+
+        n=float(1)
+        N=float(N)
+        MAXn=0.5+math.sqrt(1+8*N)/2
+
+        while(n<=MAXn):
+            a=float(N/n-n/2+0.5)
+            if (int(a)==a) and (a>0):
+                number=number+1
+            n=n+1
+
+        return number
+
 A=Solution()
-print A.consecutiveNumbersSum(16)
+print A.consecutiveNumbersSum2(16)
